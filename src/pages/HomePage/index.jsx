cdf0b1a61e076ba17a5ce1f37199f1a09b602eb5
Kristýna Konečná <kristyna.konecna02@gmail.com>
2024-05-18 17:29:22 +0200

    volani API

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -10,11 +10,35 @@ export const HomePage = () => {
     setJourney(journeyData);
   };
 
+  const handleBuy = async () => {
+
+    const resp = await fetch('https://apps.kodim.cz/daweb/leviexpress/api/reservation', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        action: 'create',
+        seat: journey.autoSeat,
+        journeyId: journey.journeyId,
+      }),
+    });
+    const data = await resp.json();
+    console.log(data.results.reservationId)
+  }
+  
+  
+
   return (
     <main>
       <JourneyPicker value={journey} onJourneyChange={handleJourneyChange} />
       {journey !== null ? <JourneyDetail journey={journey} /> : ''}
       {journey !== null ? <SelectedSeat number={journey.autoSeat} /> : ''}
+
+      <div className="controls container">
+      <button className="btn btn--big" type="button" onClick={handleBuy}>Rezervovat</button>
+      </div>
+
     </main>
   );
 };
