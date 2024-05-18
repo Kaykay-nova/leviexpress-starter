b23a9c147cf4ac0c6c2a50e6dc307b94f207f62d
Patricie Vyhlídalová <patina.vy@gmail.com>
2024-05-18 16:28:51 +0200

    zobrazení sedadla

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,8 +1,7 @@
 import { useState } from 'react';
 import { JourneyPicker } from '../../components/JourneyPicker';
 import { JourneyDetail } from '../../components/JourneyDetail';
-
-
+import { SelectedSeat } from '../../components/SelectedSeat';
 
 export const HomePage = () => {
   const [journey, setJourney] = useState(null);
@@ -14,7 +13,8 @@ export const HomePage = () => {
   return (
     <main>
       <JourneyPicker value={journey} onJourneyChange={handleJourneyChange} />
-      {journey !== null ? <JourneyDetail journey={journey}/> : ''}
+      {journey !== null ? <JourneyDetail journey={journey} /> : ''}
+      {journey !== null ? <SelectedSeat number={journey.autoSeat} /> : ''}
     </main>
   );
 };
