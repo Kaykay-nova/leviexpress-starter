efd87cb82758ad08604ff0a7e9ebeecb994e6951
Patricie Vyhlídalová <patina.vy@gmail.com>
2024-05-18 10:14:29 +0200

    založení komponenty ReservationPage

---
+++ src/components/ReservationPage/index.jsx
@@ -0,0 +1,10 @@
+import React, { useEffect, useState } from 'react';
+
+export const ReservationPage = () => (
+  <div className="container">
+    <h2>
+      Vaše e-jízdenka
+      {/* <a href="/">LeviExpress</a> */}
+    </h2>
+  </div>
+);
